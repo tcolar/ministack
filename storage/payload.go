aaac87185eb124274236c0e8e10a1fe078214a5c
Thibaut Colar <tcolar@colar.net>
2019-08-13 17:12:28 -0700

    Better data payload

---
+++ storage/payload.go
@@ -0,0 +1,23 @@
+package storage
+
+import "fmt"
+
+// MessagePayload represents how a message is stored in the DB
+type MessagePayload struct {
+	UUID       string
+	SequenceID uint64
+	CreatedAt  int64
+	Payload    string
+	// SendAT ??
+}
+
+// PayloadKey helps creating sortable keys for the payload
+type PayloadKey struct {
+	Timestamp int64
+	Sequence  uint64 // in case of ties on timestamp
+}
+
+// Byte ordered key (sortable)
+func (p *PayloadKey) String() string {
+	return fmt.Sprintf("%010d_%019d", p.Timestamp, p.Sequence)
+}
